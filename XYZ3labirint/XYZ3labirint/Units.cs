54212488c5ac21cb4b92b56c818d10a7d12a8139
VSUS <152472758+VSUSnator@users.noreply.github.com>
2024-11-28 13:42:26 +0800

    Add files via upload

---
+++ XYZ3labirint/XYZ3labirint/Units.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace MazeTemplate
+{
+    public class Units : IEnumerable
+    {
+        private HashSet<Unit> _units = new();
+
+        public void Add(Unit unit)
+        {
+            _units.Add(unit);
+        }
+
+        public void Remove(Unit unit)
+        {
+            _units.Remove(unit);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Unit unit in _units)
+            {
+                yield return unit;
+            }
+        }
+    }
+}
---
+++ XYZ3labirint/XYZ3labirint/VerticalObstacle.cs
@@ -0,0 +1,26 @@
+﻿namespace MazeTemplate
+{
+    public class VerticalObstacle : Unit
+    {
+        private bool _obstacleDownDir = true;
+
+        public VerticalObstacle(Vector2 startPosition, char symbol, ConsoleRenderer renderer) :
+            base(startPosition, symbol, renderer)
+        {
+        }
+
+        public override void Update()
+        {
+            if (_obstacleDownDir)
+            {
+                if (!TryMoveDown())
+                    _obstacleDownDir = false;
+            }
+            else
+            {
+                if (!TryMoveUp())
+                    _obstacleDownDir = true;
+            }
+        }
+    }
+}
